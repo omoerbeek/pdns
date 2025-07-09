dfa35d6d5adee624ac6bef7b8728eed4e37a3b18
Otto Moerbeek <otto.moerbeek@open-xchange.com>
2025-07-09 13:52:31 +0200

    rec: remove old style logging calls from rec specific code
    Signed-off-by: Otto Moerbeek <otto.moerbeek@open-xchange.com>

--- pdns/recursordist/ratelimitedlog.hh
+++ pdns/recursordist/ratelimitedlog.hh
@@ -38,10 +38,9 @@ public:
   {
     uint32_t count{};
     if (doLog(count)) {
-      SLOG(g_log << Logger::Error << msg << " created an exception" << endl,
-           slog->info(Logr::Error, msg + " created an exception",
-                      "ratelimitingSkipped", Logging::Loggable(count),
-                      "exception", Logging::Loggable("other"), args...));
+      slog->info(Logr::Error, msg + " created an exception",
+                 "ratelimitingSkipped", Logging::Loggable(count),
+                 "exception", Logging::Loggable("other"), args...);
     };
   }
 
@@ -50,10 +49,9 @@ public:
   {
     uint32_t count{};
     if (doLog(count)) {
-      SLOG(g_log << Logger::Error << msg << " created an exception: " << stdException.what() << endl,
-           slog->error(Logr::Error, stdException.what(), msg + " created an exception",
-                       "ratelimitingSkipped", Logging::Loggable(count),
-                       "exception", Logging::Loggable("std::exception"), args...));
+      slog->error(Logr::Error, stdException.what(), msg + " created an exception",
+                  "ratelimitingSkipped", Logging::Loggable(count),
+                  "exception", Logging::Loggable("std::exception"), args...);
     }
   }
 
@@ -62,10 +60,9 @@ public:
   {
     uint32_t count{};
     if (doLog(count)) {
-      SLOG(g_log << Logger::Error << msg << " created an PDNSException: " << pdnsException.reason << endl,
-           slog->error(Logr::Error, pdnsException.reason, msg + " created an exception",
-                       "ratelimitingSkipped", Logging::Loggable(count),
-                       "exception", Logging::Loggable("PDNSException"), args...));
+      slog->error(Logr::Error, pdnsException.reason, msg + " created an exception",
+                  "ratelimitingSkipped", Logging::Loggable(count),
+                  "exception", Logging::Loggable("PDNSException"), args...);
     }
   }
 
